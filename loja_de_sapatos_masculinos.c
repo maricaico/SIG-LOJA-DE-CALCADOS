44ab72b97826eec58e2fbd4e1c96e674b778c6ab
Mariana <mari_caico@yahoo.com.br>
2023-08-21 02:27:54 -0300

    Altera informações adicionais sobre o programa

--- loja_de_sapatos_masculinos.c
+++ loja_de_sapatos_masculinos.c
@@ -28,11 +28,11 @@ void tela_principal(void) {
     printf("***               Departamento de Computação e Tecnologia                   ***\n");
     printf("***                  Disciplina DCT1106 -- Programação                      ***\n");
     printf("***                  Projeto Loja de Sapatos Masculinos                     ***\n");
-    printf("***             Developed by @maricaico  -- since Ago, 2023                 ***\n");
+    printf("***             Developed by @maricaico  -- since Agosto, 2023              ***\n");
     printf("***                                                                         ***\n");
     printf("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=\n");
     printf("***                                                                         ***\n");
-    printf("***            = = = = = Sistema de Gestão Escolar = = = = =                ***\n");
+    printf("***       **** SISTEMA DE GESTÃO PARA LOJA DE CALÇADOS MASCULINOS ****      ***\n");
     printf("***                                                                         ***\n");
     printf("***            1. Cadastro                                                  ***\n");
     printf("***            2. Vendas                                                    ***\n");
@@ -48,14 +48,14 @@ void tela_principal(void) {
 void tela_sobre(void) {
     printf("\n");
     printf("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=\n");
-    printf("///                                                                         ///\n");
-    printf("///     **** SISTEMA DE GESTÃO PARA LOJA DE CALÇADOS MASCULINOS ****        ///\n");
-    printf("///                                                                         ///\n");
-    printf("///  Programa desenvolvido para a disciplina DCT1106 - Programação, e será  ///\n");
-    printf("///  usado como requisito de conclusão da disciplina. O programa contém as  ///\n");
-    printf("///  principais funcionalidades necessárias para o controle  e a gestão de  ///\n");
-    printf("///  vendas e de estoque de uma loja de sapatos masculinos.                 ///\n");
-    printf("///                                                                         ///\n");
+    printf("***                                                                         ***\n");
+    printf("***     **** SISTEMA DE GESTÃO PARA LOJA DE CALÇADOS MASCULINOS ****        ***\n");
+    printf("***                                                                         ***\n");
+    printf("***  Programa desenvolvido para a disciplina DCT1106 - Programação, e será  ***\n");
+    printf("***  usado como requisito de conclusão da disciplina. O programa contém as  ***\n");
+    printf("***  principais funcionalidades necessárias para o controle, e a gestão de  ***\n");
+    printf("***  vendas e de estoque de uma loja de sapatos masculinos.                 ***\n");
+    printf("***                                                                         ***\n");
     printf("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=\n");
     printf("\n");
 }
@@ -64,15 +64,15 @@ void tela_sobre(void) {
 void tela_equipe(void) {
     printf("\n");
     printf("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=\n");
-    printf("///                                                                         ///\n");
-    printf("///      **** SISTEMA DE GESTÃO PARA LOJA DE CALÇADOS MASCULINOS ****       ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            Este projeto foi desenvolvido por:                           ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            Mariana dos Santos Batista Medeiros                          ///\n");
-    printf("///            E-mail: mari_caico@yahoo.com.br                              ///\n");
-    printf("///            Git: https://github.com/maricaico                            ///\n");
-    printf("///                                                                         ///\n");
+    printf("***                                                                         ***\n");
+    printf("***      **** SISTEMA DE GESTÃO PARA LOJA DE CALÇADOS MASCULINOS ****       ***\n");
+    printf("***                                                                         ***\n");
+    printf("***            Este projeto foi desenvolvido por:                           ***\n");
+    printf("***                                                                         ***\n");
+    printf("***            Mariana dos Santos Batista Medeiros                          ***\n");
+    printf("***            E-mail: mari_caico@yahoo.com.br                              ***\n");
+    printf("***            Git: https://github.com/maricaico                            ***\n");
+    printf("***                                                                         ***\n");
     printf("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=\n");
     printf("\n");
 } 
